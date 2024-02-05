21f458278753718b3621a49c0e78a683a62859a6
kitochio <mtakaba84@gmail.com>
2024-02-05 14:43:10 +0900

    ピッケルを持たせる

--- src/main/java/command/MiningGameStart.java
+++ src/main/java/command/MiningGameStart.java
@@ -1,9 +1,12 @@
 package command;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class MiningGameStart implements CommandExecutor {
 
@@ -11,8 +14,12 @@ public class MiningGameStart implements CommandExecutor {
   public boolean onCommand(CommandSender commandSender, Command command, String s,
       String[] strings) {
     if (commandSender instanceof Player player) {
+      PlayerInventory inventory = player.getInventory();
+
       player.setHealth(20);
       player.setFoodLevel(20);
+      inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
+
     }
     return false;
   }
