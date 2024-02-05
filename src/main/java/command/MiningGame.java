1677b85b04a2a72c6b55b16906f2b572428315ca
kitochio <mtakaba84@gmail.com>
2024-02-05 19:33:19 +0900

    開始時にダイヤピッケル、暗視を付与

--- src/main/java/command/MiningGame.java
+++ src/main/java/command/MiningGame.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +14,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class MiningGame implements CommandExecutor, Listener {
 
@@ -22,11 +26,8 @@ public class MiningGame implements CommandExecutor, Listener {
   public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
     if (commandSender instanceof Player player) {
       this.player = player;
-      PlayerInventory inventory = player.getInventory();
 
-      player.setHealth(20);
-      player.setFoodLevel(20);
-      inventory.setItemInMainHand(new ItemStack(Material.DIAMOND_PICKAXE));
+      initPlayerStatus(player);
     }
     return false;
   }
@@ -44,5 +45,25 @@ public class MiningGame implements CommandExecutor, Listener {
     }
   }
 
+  private static void initPlayerStatus(Player player) {
+    PlayerInventory inventory = player.getInventory();
+    player.setHealth(20);
+    player.setFoodLevel(20);
+    inventory.setItemInMainHand(enchantDiaPic());
+    player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 6000, 0));
+  }
+
+  private static ItemStack enchantDiaPic() {
+    ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
+    ItemMeta meta = item.getItemMeta();
+    if (Objects.isNull(meta)) {
+      return null;
+    }
+    meta.addEnchant(Enchantment.DIG_SPEED, 5, true); //効率強化V
+    meta.addEnchant(Enchantment.DURABILITY, 3, true); //耐久力III
+    meta.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 3, true); //幸運III
+    item.setItemMeta(meta);
+    return item;
+  }
 
 }
